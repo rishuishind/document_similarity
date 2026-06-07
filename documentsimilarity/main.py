ce17de44ca2744c90c3a0fd1c01e6988a686b5b0
rishuishind <hespnod2348@gmail.com>
2026-06-07 18:16:14 +0530

    finding cosine similarity

--- documentsimilarity/main.py
+++ documentsimilarity/main.py
@@ -12,4 +12,16 @@ documents = [
     "Jasprit Bumrah is an Indian fast bowler known for his unorthodox action and yorkers."
 ]
 
-doc_embedding = embedding.embed_documents(documents)
+try:
+    doc_embedding = embedding.embed_documents(documents)
+
+    user_query = input("Enter the query you want to search for: \n")
+    query_embedding = embedding.embed_query(user_query)
+
+    # Now we will find the cosine similarity between document and user query
+    scores = cosine_similarity([query_embedding],doc_embedding)[0]
+    index,score = max(enumerate(scores),key=lambda x:x[1])
+    print(documents[index])
+
+except Exception as e:
+    print(f"Error while fetching as {e}")
